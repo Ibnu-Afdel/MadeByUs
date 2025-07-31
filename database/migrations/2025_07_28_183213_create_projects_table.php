34c490e5bb4baffa8bc3ee6a98c73afe0ebc178f
ibnuafdel <ibnuafdel@gmail.com>
2025-07-31 12:57:28 +0300

    feat: Modify prokects table to more comprehensive table

--- database/migrations/2025_07_28_183213_create_projects_table.php
+++ database/migrations/2025_07_28_183213_create_projects_table.php
@@ -15,9 +15,14 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->boolean('is_featured')->default(false);
             $table->string('title');
+            $table->string('slug')->unique();
             $table->text('description');
             $table->string('status')->default('pending');
+            $table->unsignedInteger('view_count')->default(0);
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('is_priority')->default(false);
             $table->timestamps();
         });
     }
