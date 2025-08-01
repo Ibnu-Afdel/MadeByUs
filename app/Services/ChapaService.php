ac495589d7e7a7680c1fddf2638f857ffbd11374
ibnuafdel <ibnuafdel@gmail.com>
2025-08-02 00:53:46 +0300

    feat: Add initialize and verify payment

--- app/Services/ChapaService.php
+++ app/Services/ChapaService.php
@@ -3,6 +3,10 @@
 namespace App\Services;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Str;
+
+use function Pest\Laravel\json;
 
 class ChapaService
 {
@@ -28,4 +32,42 @@ class ChapaService
         ]);
     }
 
+    public function initializePayment($data)
+    {
+        $txRef = Str::uuid()->toString();
+
+        $payload  = array_merge($data, [
+            'tx_ref' => $txRef,
+            'currency' => 'ETB',
+            'callback_uri' => config('chapa.callback_url'),
+            'return_url' => config('app.url') . '/pro/thank-you',
+        ]);
+        try {
+            $response = $this->client->post('/transaction/initialize', [
+                'json' => $payload
+            ]);
+            $result = json_decode($response->getBody()->getContents(), true);
+
+            if (($result['status'] ?? '') === 'success'){
+                return $result['data']['checkout_url'];
+            }
+            throw new \Exception($result['message'] ?? 'Failed to initialize Chapa payment');
+        } catch(RequestException $e){
+            throw new \Exception('Chapa result failed.');
+        }
+    }
+
+    public function verifyTransaction($txRef)
+    {
+        try{
+            $response = $this->client->get("transaction/verify/{$txRef}");
+            $body = json_decode($response->getBody(), true);
+            return $body;
+        } catch(\Exception $e){
+            return [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ];
+        }
+    }
 }
